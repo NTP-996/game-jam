779d38a41d7f56ee9f30de193b5d633583165ae4
Belacosaur <calebjmartin@hotmail.com>
2025-07-23 11:46:09 +0700

    Updates

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -21,48 +21,37 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer__container">
-        <nav className="footer__nav" aria-label="Footer navigation">
-          <a href="#home" onClick={handleNavClick}>Home</a>
-          <a href="#overview" onClick={handleNavClick}>Overview</a>
 
-          <a href="#schedule" onClick={handleNavClick}>Schedule</a>
-          <a href="#judging" onClick={handleNavClick}>Judging Criteria</a>
-          <a href="#faq" onClick={handleNavClick}>FAQ</a>
-        </nav>
 
-        <div className="footer__logos">
-          <a href="#" className="logo superteam-logo">
-            <Image src="/assets/logos/logo.svg" alt="Superteam VN Logo" width={200} height={80} />
-          </a>
-        </div>
+
 
         <div className="footer__sponsors">
           <div className="footer__sponsors-title">
-            <Image src="/assets/text/sponsors.svg" alt="Sponsor" width={200} height={60} />
+            <Image src="/assets/text/sponsors.svg" alt="Sponsor" width={200} height={60} style={{margin: '0 auto', display: 'block'}} />
           </div>
           <div className="footer__sponsors-grid">
             <div className="footer__sponsor-item">
               <Image
                 src="/assets/logos/superteam.svg"
                 alt="Superteam"
-                width={150}
-                height={60}
+                width={100}
+                height={40}
               />
             </div>
             <div className="footer__sponsor-item">
               <Image
                 src="/assets/sponsor/honeycomb.jpg"
                 alt="Honeycomb Protocol"
-                width={120}
-                height={40}
+                width={80}
+                height={30}
               />
             </div>
             <div className="footer__sponsor-item">
               <Image
                 src="/assets/partner/sendlogo.png"
                 alt="Send Arcade"
-                width={150}
-                height={60}
+                width={100}
+                height={40}
               />
             </div>
           </div>
